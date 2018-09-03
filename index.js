85de48d92649465c3a40c1b81eceaffedfeebd31
Noven Temer <noven5823@gmail.com>
2018-09-03 19:01:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 
+class Driver{
+	constructor(name, startDate){
+		this.name = name;
+		this.startDate = new Date(startDate);
+	}
+	yearsExperienceFromBeginningOf(year){
+		return year - (this.startDate).getFullYear()
+	}
+}
+
+
+class Route{
+	constructor(beginningLocation, endingLocation){
+		this.beginningLocation = beginningLocation;
+		this.endingLocation = endingLocation;
+	}
+
+	blocksTravelled(){
+		let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+		var endHori = eastWest.indexOf(this.endingLocation.horizontal);
+		var begHori = eastWest.indexOf(this.beginningLocation.horizontal);
+
+
+		var start = this.beginningLocation.vertical;
+		var end = this.endingLocation.vertical;
+
+
+         if(end > start && endHori > begHori){
+			return (endHori - begHori)+ (end - start)
+		}else if(end > start){
+			return end - start;
+		}else{
+			return endHori - begHori
+		}
+
+	}
+
+	estimatedTime(peak){
+		if(peak){
+			return this.blocksTravelled()/2
+		}else{
+			return this.blocksTravelled()/3
+		}
+	}
+}
