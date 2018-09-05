2a8fc1b9ce328e382b6c930c0e4bdcbe36c0f892
Ahmed kamil jemil <akjemil@gmail.com>
2018-09-05 19:07:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+class Driver{
+   constructor(name,startDate){
+     this.name=name;
+     this.startDate=new Date(startDate);
+   }
+   yearsExperienceFromBeginningOf(y){
+    let sum = new Date(y, 1, 1);
+    let totalsum = (sum - this.startDate)/(365*24*60*60*1000);
+    return parseInt(totalsum);
+  }
+}
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+}
